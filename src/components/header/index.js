2c961de9b424aa34f55dffc8de99e43f15f03c5e
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-17 22:20:50 +0800

    finish header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,28 +1,36 @@
 import logo from "../../assets/img/logo.svg"
 import Image from "next/image"
 import Link from "next/link"
+import { use, useState } from "react"
 
 export default function Header() {
+  const [open, setOpen] = useState(false)
+  function handleNavOpen() {
+    setOpen(!open)
+  }
   return (
-    <header className="header">
+    <header className={`header ${open ? "nav_open" : ""}`}>
       <Image src={logo} alt="logo" height={25} className="header__img" />
-      <nav>
+      <nav className="nav">
         <ul className="nav__list" role="list">
-          <li>
+          <li className="nav__item">
             <button disabled className="btn nav__btn">
               邀請他人簽署
             </button>
           </li>
-          <li>
+          <li className="nav__item">
             <Link href="/" className="nav__btn">
               簽署新文件
             </Link>
           </li>
-          <li>
+          <li className="nav__item">
             <button className="btn nav__btn">登入</button>
           </li>
         </ul>
       </nav>
+      <button className="btn header__toggle" onClick={handleNavOpen}>
+        {/* TODO 之後使用SVG呈現 */}三
+      </button>
     </header>
   )
 }
