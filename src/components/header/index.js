851f43c6984bd28a5b89240637ae04471975247c
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-18 14:35:52 +0800

    fix header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import logo from "../../assets/img/logo.svg"
 import Image from "next/image"
 import Link from "next/link"
-import { use, useState } from "react"
+import { useState } from "react"
 
 export default function Header() {
   const [open, setOpen] = useState(false)
@@ -10,27 +10,29 @@ export default function Header() {
   }
   return (
     <header className={`header ${open ? "nav_open" : ""}`}>
-      <Image src={logo} alt="logo" height={25} className="header__img" />
-      <nav className="nav">
-        <ul className="nav__list" role="list">
-          <li className="nav__item">
-            <button disabled className="btn nav__btn">
-              邀請他人簽署
-            </button>
-          </li>
-          <li className="nav__item">
-            <Link href="/" className="nav__btn">
-              簽署新文件
-            </Link>
-          </li>
-          <li className="nav__item">
-            <button className="btn nav__btn">登入</button>
-          </li>
-        </ul>
-      </nav>
-      <button className="btn header__toggle" onClick={handleNavOpen}>
-        {/* TODO 之後使用SVG呈現 */}三
-      </button>
+      <div className="header__container">
+        <Image src={logo} alt="logo" height={25} className="header__img" />
+        <nav className="nav">
+          <ul className="nav__list" role="list">
+            <li className="nav__item">
+              <button disabled className="btn nav__btn">
+                邀請他人簽署
+              </button>
+            </li>
+            <li className="nav__item">
+              <Link href="/" className="nav__btn">
+                簽署新文件
+              </Link>
+            </li>
+            <li className="nav__item">
+              <button className="btn nav__btn">登入</button>
+            </li>
+          </ul>
+        </nav>
+        <button className="btn header__toggle" onClick={handleNavOpen}>
+          {/* TODO 之後使用SVG呈現 */}三
+        </button>
+      </div>
     </header>
   )
 }
